b94ee7c0ea217fe863bd7a4c6459cd4505bb808b
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-07-05 01:13:01 +0200

    Add Fly Madonne

--- js/script.js
+++ js/script.js
@@ -2,8 +2,47 @@ const { createApp } = Vue ;
 
 createApp({
     data(){
-        return{
-            message : 'Ciao mamma guarda come mi diverto!'
+        return {
+            newTodo:'' ,
+            todos: [
+                {
+                    text : ' volano madonne ',
+                    done : true ,
+                },
+                {
+                    text : ' volano madonne ',
+                    done : true ,
+                },
+                {
+                    text : ' volano madonne ',
+                    done : true ,
+                },
+                {
+                    text : ' volano madonne ',
+                    done : true ,
+                },
+
+                ]
+            }
+
+        },
+    methods:{
+        addTodo(){
+            // if this.newTodo.length > 0
+            //.trim serve per togliere gli spazi in una stringa
+            if(this.newTodo.trim() != ' '){
+            const fraDiCorpo = { text : this.newTodo,
+            done : ' true ' };
+            this.todos.push(fraDiCorpo); 
+            this.newTodo= " " ;
+            }
+           
+        } , 
+        removeTodo(index){
+            console.log('Rimuovi classe' , index);
+            // per eliminare un oggetto da un array usiamo splice
+            this.todos.splice(index , 1);
         }
+    
     }
-}).mount('#app')
+}).mount('#app');
